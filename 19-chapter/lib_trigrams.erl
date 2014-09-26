ea7470c3d87402e4683be3edfe74450248157780
litaotao <charles.upboy@gmail.com>
2014-09-26 10:45:05 +0800

    19

--- 19-chapter/lib_trigrams.erl
+++ 19-chapter/lib_trigrams.erl
@@ -1,6 +1,8 @@
 -module (lib_trigrams).
 -export ([for_each_trigram_in_the_english_language/2,
-		  make_tables/0, timer_tests/0]).
+		  make_tables/0, timer_tests/0, is_word/2,
+		  open/0, close/1, is_this_a_trigram/2,
+		  how_many_trigrams/0]).
 -import (list, [reverse/1]).
 
 make_tables() ->
@@ -15,7 +17,6 @@ make_tables() ->
 	{Micro4, _} = timer:tc(?MODULE, make_mod_set, []),
 	FileSize3 = filelib:file_size("trigrams.set"),
 	io:format("Module sets size = ~p, time/trigram = ~p~n", [FileSize3/Ntri, Micro4/N]).
-
 
 make_ets_ordered_set() -> make_a_set(ordered_set, "trigrams0S.tab").
 make_ets_set() -> make_a_set(set, "trigramsS.tab").
@@ -35,23 +36,36 @@ make_mod_set() ->
 	D1 = for_each_trigram_in_the_english_language(F, D),
 	file:write_file("trigrams.set", [term_to_binary(D1)]).
 
+%%%
 timer_tests() ->
 	time_lookup_ets_set("Ets ordered set", "trigramsOS.tab"),
 	time_lookup_ets_set("Ets set", "trigramsS.tab"),
 	time_lookup_module_sets().
 
+time_lookup_ets_set(Type, File) ->
+	{ok, Tab} = ets:file2tab(File),
+	L = ets:tab2list(Tab),
+	Size = length(L),
+	{M, _} = timer:tc(?MODULE, lookup_all_set, [Tab, L]),
+	io:format("~s lookup=~p micro seconds~n", [Type, M/Size]),
+	ets:delete(Tab).
+
 time_lookup_module_sets()->
 	{ok, Bin} = file:read_file("trigrams.set"),
 	Set = binary_to_term(Bin),
 	Keys = sets:to_list(Set),
-	Size = len(Keys),
+	Size = length(Keys),
 	{M, _} = timer:tc(?MODULE, lookup_all_set, [Set, Keys]),
 	io:format("Module set lookup = ~p micro seconds~n", [M/Size]).
 
 lookup_all_set(Set, L) ->
 	lists:foreach(fun(Key) -> sets:is_element(Key, Set) end, L).
 
+how_many_trigrams() ->
+	F = fun(_, N) -> 1+N end,
+	for_each_trigram_in_the_english_language(F, 0).
 
+%%%
 for_each_trigram_in_the_english_language(F, A0) ->
 	{ok, Bin0} = file:read_file("words.ngl.gz"),
 	Bin = zlib:gunzip(Bin0),
@@ -79,7 +93,29 @@ scan_trigrams([X, Y, Z|T], F, A) ->
 scan_trigrams(_, _, A) ->
 	A.
 
+%%%
+is_word(Tab, Str) ->  is_word1(Tab, "\s"++Str++"\s").
+is_word1(Tab, [_,_,_]=X) -> is_this_a_trigram(Tab, X);
+is_word1(Tab, [A,B,C|D]) ->
+	case is_this_a_trigram(Tab, [A,B,C]) of
+		true -> is_word1(Tab, [B,C|D]);
+		false -> false
+	end;
+is_word1(_, _) ->
+	false.
 
+is_this_a_trigram(Tab, X) ->	
+	case ets:lookup(Tab, list_to_binary(X)) of
+		[] -> false;
+		_  -> true
+	end.
+
+open() -> 
+	{ok, I} = ets:file2tab(filename:dirname(code:which(?MODULE))) ++ "/trigramsS.tab",
+	I.
+
+close(Tab) ->
+	ets:delete(Tab).
 
 
 
