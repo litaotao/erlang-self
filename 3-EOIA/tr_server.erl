999cf16aaac8cae22d5881c21efacea0369e3b79
litaotao <charles.upboy@gmail.com>
2014-10-14 14:44:44 +0800

    update

--- 3-EOIA/tr_server.erl
+++ 3-EOIA/tr_server.erl
@@ -74,4 +74,44 @@ handle_call(get_count, _From, State) ->
 handle_cast(stop, State) ->
 	{stop, normal, State}.	
 
+handle_info({tcp, Socket, RawData}, State) ->
+	do_rpc(Socket, RawData),
+	RequestCount = State#state.request_count,
+	{noreply, State#state{request_count = RequestCount+1}};
 
+handle_info(timeout, #state{lsock = LSock} = State) ->
+	{ok, _Sock} = gen_tcp:accept(LSock),
+	{noreply, State}.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+
+%%%------------------------------------------
+%%% Internal functions
+%%%------------------------------------------
+do_rpc(Socket, RawData) ->
+	try
+		{M, F, A} = split_out_mfa(RawData),
+		Result = apply(M, F, A),
+		gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Result]))
+	catch
+		_Class:Err ->
+			gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Err]))
+	end.
+
+split_out_mfa(RawData) ->
+	MFA = re:replace(RawData, "\r\n$", "", [{return, list}]),
+	{match, [M, F, A]} = 
+		re:run(MFA, 
+			   "(.*):(.*)\s*\\((.*)\s*\\)\s*.\s*$",
+			   [{capture, [1,2,3], list}, ungreedy]),
+	{list_to_atom(M), list_to_atom(F), args_to_terms(A)}
+
+args_to_terms(RawArgs) ->
+	{ok, Toks, _Line} = erl_scan:string("[" ++ RawArgs ++"]. ", 1),
+	{ok, Args} = erl_parse:parse_term(Toks),
+	Args.
